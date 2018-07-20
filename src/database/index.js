03b390e9583270a6ba8d4ec2945a7da31a2fe61f
Pedro Diniz <pedro.caetano@za9.com.br>
2018-07-20 14:02:28 -0300

    initial-commit

---
+++ src/database/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/api-modelo-teste'
+
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
+
